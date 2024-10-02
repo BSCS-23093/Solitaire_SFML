4edac0c04cb6d310e38312e2e02de80e822ee6f3
BSCS-23093 <149577887+BSCS-23093@users.noreply.github.com>
2024-10-02 12:54:18 -0700

    Update deck.h

--- deck.h
+++ deck.h
@@ -8,11 +8,11 @@ using namespace sf;
 class card_deck {
 private:
     vector<cards*> deck;
-    int size=52;
+    int size = 52;
 public:
     card_deck() {
         //
-        
+
         string a = "textures/heart/1_heart.png";
         string b = "textures/diamond/1_diamond.png";
         string c = "textures/ace/1_ace.png";
@@ -30,7 +30,7 @@ public:
             b.replace(17, 1, to_string(i + 1));
             c.replace(13, 1, to_string(i + 1));
             d.replace(15, 1, to_string(i + 1));
-            
+
             //
             //de.add_card(cards(i, "Hearts", a, "red"));
             //de.add_card(cards(i, "Diamonds", b, "red"));
@@ -48,20 +48,20 @@ public:
         string c2 = "textures/ace/10_ace.png";
         string d2 = "textures/spade/10_spade.png";
         for (int i = 0; i <= 3; i++) {
-            cards* Hearts2 = new cards(i+10, "Hearts", a2, "red");
+            cards* Hearts2 = new cards(i + 10, "Hearts", a2, "red");
             add_card(Hearts2);
-            cards* Diamonds2 = new cards(i+10, "Diamonds", b2, "red");
+            cards* Diamonds2 = new cards(i + 10, "Diamonds", b2, "red");
             add_card(Diamonds2);
-            cards* Aces2 = new cards(i+10, "Aces", c2, "black");
+            cards* Aces2 = new cards(i + 10, "Aces", c2, "black");
             add_card(Aces2);
-            cards* Spade2 = new cards(i+10, "Spades", d2, "black");
+            cards* Spade2 = new cards(i + 10, "Spades", d2, "black");
             add_card(Spade2);
-            a2.replace(16, 1, to_string(i+1));
-            b2.replace(18, 1, to_string(i+1));
-            c2.replace(14, 1, to_string(i+1));
-            d2.replace(16, 1, to_string(i+1));
+            a2.replace(16, 1, to_string(i + 1));
+            b2.replace(18, 1, to_string(i + 1));
+            c2.replace(14, 1, to_string(i + 1));
+            d2.replace(16, 1, to_string(i + 1));
         }
-        
+
         /*
         string a = "textures/heart/1_heart.png";
         string b = "textures/diamond/1_diamond.png";
@@ -101,32 +101,35 @@ public:
         }
     }
     void display(RenderWindow& window) {
-        for (int i = 0,k=0; i < deck.size(); i++,k+=30) {
+        for (int i = 0, k = 0; i < deck.size(); i++, k += 30) {
             deck[i]->set_position(100, 140);
             deck[i]->draw(window);
             //k = k % 800;
         }
     }
-	//displaying not working fine displays white rectangle instead of loaded texture
-	void draw(RenderWindow& window) {
-		for (int i = 0; i < deck.size(); i++) {
-			deck[i]->draw(window);
-		}
+    //displaying not working fine displays white rectangle instead of loaded texture
+    void draw(RenderWindow& window) {
+        for (int i = 0; i < deck.size(); i++) {
+            deck[i]->draw(window);
+        }
+    }
+    void add_card(cards* c) {
+        deck.push_back(c);
+    }
+    void remove_card_last() {
+        //deck.pop_back();
+    }
+    void get_sprite(int i) {
+        deck[i]->get_sprite();
+    }
+	bool isEmpty() {
+		return deck.empty();
 	}
-	void add_card(cards* c) {
-		deck.push_back(c);
-	}
-	void remove_card_last() {
-		//deck.pop_back();
-	}
-	void get_sprite(int i) {
-		deck[i]->get_sprite();
-	}
-	cards* get_card() {
-		cards* temp = deck.back();
-		deck.pop_back();
-		return temp;
-	}
+    cards* get_card() {
+        cards* temp = deck.back();
+        deck.pop_back();
+        return temp;
+    }
     cards* operator[](int i) const {
         return deck[i];
     }
